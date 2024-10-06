a9374e557e55a0350e4b9d459a37ba95bf92b7e2
kk722cry <kk722crypto@gmail.com>
2024-10-06 17:12:46 +0900

    add: langgraph、分岐、ループ

--- app/generate_control.py
+++ app/generate_control.py
@@ -1,29 +1,101 @@
+import asyncio
+from langgraph.graph import StateGraph
+from typing import TypedDict, List
 
 
+# --- graphに流すstate ---
+class TextState(TypedDict):
+    text: str
+    forbidden_words: List[str]
+    next_action: str
+    count: int
+    is_changed: bool
 
 
-class GenerateControl:
-    @staticmethod
-    def control_forbidden_word(prompt: str, key: str):
-        #DBから取ってきたような気持ち
-        l = ["黒烏龍茶", "パッションティー", "蜂蜜入り"]
-        l = str(l)
-        #追加するプロンプト
-        fw_prompt = f"これらのワードは生成に含めないでください:{l}"
-        fw_prompt = fw_prompt.format(l)
-        return fw_prompt + prompt
+# --- node内で使用する関数 ---
+def _decide_next_action(state: TextState):
+    print("decide next action")
 
-    def control_personal_infomation(prompt: str):
-        pass
+    if any(word in state["text"] for word in state["forbidden_words"]):
+        state["next_action"] = "delete"
+    else:
+        state["next_action"] = "finalize"
+    return state
 
 
-q = "体脂肪の燃焼を促進する効果があるお茶を教えてください"
+def _delete_forbidden_words(state: TextState):
+    print("delete forbidden words")
+    
+    # 禁止用語を '*' に置き換える
+    for word in state["forbidden_words"]:
+        replacement = '*' * len(word)
+        state["text"] = state["text"].replace(word, replacement)
+    
+    state["count"] += 1
+    return state
 
-prompt = f"あなたは飲み物の知識に長けた専門家です。私の質問に答えてください。質問：{q}"
+
+def _finalize_all_actions(state: TextState):
+    print("finalize all actions")
+
+    if state["count"] >= 1:
+        state["is_changed"] = True
+    return state
 
 
-gc = GenerateControl()
-prompt = gc.control_forbidden_word(prompt, "a")
+def _routing_next_action(state: TextState):
+    print("routing next action")
 
-prompt = prompt.format(q)
-print(prompt)
+    if state["next_action"] == "delete":
+        return "delete"
+    elif state["next_action"] == "finalize":
+        return "finalize"
+    else:
+        return
+
+
+async def main():
+    state = TextState(text="黒烏龍茶はとても美味しいです", forbidden_words=["黒烏龍茶"], next_action="", count=0, is_changed=True)
+
+    # --- LangGraph ---
+    workflow = StateGraph(TextState)
+
+    # --- node ---
+    workflow.add_node(
+        "router",
+        _decide_next_action
+    )
+    workflow.add_node(
+        "delete",
+        _delete_forbidden_words
+    )
+    workflow.add_node(
+        "finalize",
+        _finalize_all_actions
+    )
+
+    # --- edge ---
+    workflow.set_entry_point("router")
+    workflow.add_conditional_edges(
+        "router",
+        _routing_next_action
+    )
+    workflow.add_edge(
+        "delete",
+        "router"
+    )
+    workflow.set_finish_point("finalize")
+
+    # --- compile ---
+    app = workflow.compile()
+
+    # 初期状態を引数として渡す
+    try:
+        result = await app.ainvoke(state)
+        print(result)
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
